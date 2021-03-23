7a4dd40dbbd9d59ce837ac90b83f828e48fda057
Vincenzo Marcovecchio <vinceoldmark@gmail.com>
2021-03-23 23:01:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import { Switch, Route, Redirect, HashRouter } from 'react-router-dom';
 import { FileUploader } from './UploadComponent';
 
 const App = () => {
   return (
     <div className="mt-2  d-flex justify-content-between align-items-start">
-      <FileUploader FILE_SIZE="1024" accept="" url="" />
+      <HashRouter basename="/">
+        <Switch>
+          <Route
+            path="/"
+            render={() => <FileUploader FILE_SIZE="1024" accept="" url="" />}
+          />
+        </Switch>
+      </HashRouter>
     </div>
   );
 };
